fa2e46c2d6b53f97841dd5521335c29c4bd739ea
Sahil <sahilsharma2000dtc@gmail.com>
2025-01-03 16:18:19 +0530

    feat: added frontend directory and folder structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+
+  return (
+    <>
+      <main>
+          <h2 className="text-center text-3xl font-semibold text-blue-500">hello this is engage lens</h2>
+      </main>
+    </>
+  )
+}
+
+export default App
