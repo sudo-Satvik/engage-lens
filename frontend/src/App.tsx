cae4b8cbca809a821a346d0e62cedc5f7025d5d0
Sahil <sahilsharma2000dtc@gmail.com>
2025-01-04 13:04:26 +0530

    feat: added shadcn UI in vite project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,7 @@
+import { Button } from "./components/ui/button"
+import Register from "./pages/components/auth/Register"
+import Signup from "./pages/components/auth/Signup"
+
 function App() {
 
 
@@ -5,6 +9,12 @@ function App() {
     <>
       <main>
           <h2 className="text-center text-3xl font-semibold text-blue-500">hello this is engage lens</h2>
+          {/* shadcn is working as this button is from shadcn UI  */}
+          <Button variant="destructive" onClick={() => alert("hello world")}>Click Me</Button>
+          <div>
+            <Register />
+            <Signup />
+          </div>
       </main>
     </>
   )
